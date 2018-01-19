d5f86e9f79337cdef23703cf11b8912a53260030
Eric Stanulis <erstanulis@gmail.com>
2018-01-18 18:13:16 -0600

    forgot to export my router

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 router.post("/", function(req, res){
     console.log(req);
     res.send("This worked");
 })
+
+module.exports = router;

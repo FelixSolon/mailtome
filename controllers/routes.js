5b60fc1f4694e842cfb2bb6d89a9f045110436d4
Eric Stanulis <erstanulis@gmail.com>
2018-01-18 18:14:32 -0600

    Added hello world

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.get("/", function(req, res){
+    res.send("Hello World!");
+})
 
 router.post("/", function(req, res){
     console.log(req);
